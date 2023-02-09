10032d2a8ded78377ab4b44f98b16a4e583a38ec
Meraj Ali <96920684+MerajAli1@users.noreply.github.com>
2023-02-09 22:08:44 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// Loop For Square?
+
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 1; j <= 5; j++) {
+//         document.write(j)
+//     }
+//     document.write(i,"<br>")
+// }
+
+
+// Loop for increasing Triangle?
+
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 2; j <= i; j++){
+//         document.write("*")
+//     }
+//     document.write("*","</br>")
+// }
+
+
+// Loop for Diamond Shape?
+
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = i; j <= 5; j++){
+//         document.write("&nbsp;&nbsp;")
+//     }
+//     for(var k=1; k<=i;k++){
+//         document.write("$")
+
+//     }
+//         for (var j = 1; j <= i; j++) {
+//             document.write("$")
+//         }
+//         document.write("<br>")
+// }
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         document.write("&nbsp;&nbsp;")
+//     }
+//     for (j = i; j <= 5; j++) {
+//         document.write("$")
+//     }
+//     for (j = i; j <= 5; j++) {
+//         document.write("$")
+//     }
+//     document.write("<br>")
+// }
